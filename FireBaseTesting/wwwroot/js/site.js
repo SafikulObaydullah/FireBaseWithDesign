d021265f37dec8d663a7ba986c30f0978b99ece6
obaydullah <obaydullah599@gmail.com>
2023-05-12 15:20:45 +0600

    id added

--- FireBaseTesting/wwwroot/js/site.js
+++ FireBaseTesting/wwwroot/js/site.js
@@ -1,3 +1,49 @@
-﻿$(document).ready(function () {
-   console.log("ready!");
+﻿//let frms = document.getElementById("form");
+//let company = document.getElementById("company");
+//let Supplier = document.getElementById("Supplier");
+//let PaymentMode = document.getElementById("PaymentMode");
+//let Currency = document.getElementById("Currency");
+let Remarks = document.getElementById("txtRemarks");
+let adjustamount = document.getElementById("adjustamount");
+let Quantity = document.getElementById("Quantity");
+let netpayable = document.getElementById("netpayable");
+let adjustnote = document.getElementById("adjustnote");
+
+console.log(company); 
+
+frms.addEventListener("submit", (e) => {
+   alert('You clicked the button');
+   e.preventDefault();
+   formValidation();
 });
+
+let formValidation = () => {
+   if (billReference.value === "") {
+      console.log("failure");
+      msg.innerHTML = "Task cannot be blank";
+   } else {
+      console.log("success");
+      msg.innerHTML = "";
+      add.setAttribute("data-bs-dismiss", "modal");
+      acceptData();
+      add.click();
+
+      (() => {
+         add.setAttribute("data-bs-dismiss", "");
+      })();
+   }
+};
+
+let acceptData = () => {
+   data.push({
+      txtRemarks: txtRemarks.value,
+      adjustamount: adjustamount.value,
+      Quantity: Quantity.value,
+      netpayable: netpayable.value,
+      adjustnote: adjustnote.value,
+   });
+
+   localStorage.setItem("data", JSON.stringify(data));
+   console.log(data);
+   createTasks();
+};
